0dc9c9b38103564bac7378e857e9a70f01ea5067
Austen Rozanski <104370144+arozanski-quativa@users.noreply.github.com>
2023-08-02 13:46:09 -0500

    Initial update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { HttpClient } from "@angular/common/http";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'recipe-book-client';
+  users: any;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    this.http.get('http://localhost:5114/api/users').subscribe({
+      next: response => this.users = response,
+      error: err => console.log(err),
+      complete: () => console.log('Request completed')
+    });
+  }
 }
